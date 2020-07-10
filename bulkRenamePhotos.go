347ac5707890f2ae58f56c2d6043b47857e1b7cf
Flemming Mahler <flemming@mahler.io>
2020-07-10 21:07:29 +0200

    small utility to bul rename all files in directory to lowercase, no underscores and no spaces. Looks for jpg, png and webp images.

---
+++ bulkRenamePhotos.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	renameDirectory := ""
+	if len(os.Args) > 1 {
+		renameDirectory = os.Args[1]
+	} else {
+		fmt.Println("Missing directory as parameter")
+		os.Exit(10)
+	}
+
+	files, err := ioutil.ReadDir(renameDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() && (strings.HasSuffix(f.Name(), ".jpg") || strings.HasSuffix(f.Name(), ".png") || strings.HasSuffix(f.Name(), ".webp")) {
+			currentName := f.Name()
+			newName := fixName(currentName)
+
+			log.Println(currentName, "->", newName)
+			err := os.Rename(renameDirectory+currentName, renameDirectory+newName)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func fixName(dirtyName string) string {
+	newName := strings.ToLower(dirtyName)
+	newName = strings.ReplaceAll(newName, " ", "-")
+	newName = strings.ReplaceAll(newName, "_", "-")
+	return newName
+}
