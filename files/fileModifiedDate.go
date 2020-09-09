2d24c47b8823a7ef6c0185ab9858ae56b0d219f6
= <=>
2020-09-09 14:40:25 +0200

    New get file modified date

---
+++ files/fileModifiedDate.go
@@ -0,0 +1,23 @@
+ package main
+
+ import (
+    "fmt"
+    "os"
+ )
+
+
+ func main() {
+
+     filename := "fileModifiedDate.go"
+
+     // get last modified time
+     file, err := os.Stat(filename)
+
+     if err != nil {
+        fmt.Println(err)
+     }
+
+     modifiedtime := file.ModTime()
+
+     fmt.Println("Last modified time : ", modifiedtime)
+ }
